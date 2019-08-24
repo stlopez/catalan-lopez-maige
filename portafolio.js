e916ce304194c494c4a8c1c0f06e4a0da55946ba
Cotepi <53983146+Cotepi@users.noreply.github.com>
2019-08-24 13:04:50 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -2,25 +2,25 @@ var trabajos = [{
     figure: "media/eym1s_catalan-lopez-maige_2019.jpg",
     alt: "banca_terciado3mm",
     title: "Banca de terciado",
-    about: "Banca de terciado de 3mm, de 1.20x0.3x0.4mts."
+    about: "Banca de terciado de 3mm, de 1.20x0.3x0.4mts.",
     url: "https://www.instagram.com/p/B1h1k3zhEVz/?igshid=1598w6k10ruq2"
 }, {
     figure: "media/boncina_lopez-maige-suarez-2018.jpg",
     alt: "bonciña",
     title: "Bonciña",
-    about: "Producto alimenticio en base a gelatina de aga-agar, açai y leche condensada, en una galleta de granola, quinoa y coco."
+    about: "Producto alimenticio en base a gelatina de aga-agar, açai y leche condensada, en una galleta de granola, quinoa y coco.",
     url:
 }, {
     figure: "media/insectnativ-catalan-2019.jpg",
     alt: "insecta nativa",
     title: "Insecta Nativa",
-    about: "Estampado en tela para serígrafia, 120x100cms."
+    about: "Estampado en tela para serígrafia, 120x100cms.",
     url: 
 }, {
     figure: "media/myp1s_bonnefoy-lobos-lopez-ramirez-saldivia_2017.jpg",
     alt: "Descripción de la imagen",
     title: "Mobiliario público",
-    about: "Raulí y acero, instalado en Puente Pedro de Valdivia."
+    about: "Raulí y acero, instalado en Puente Pedro de Valdivia.",
     url: 
 }, {
     figure: "media/alcatraz-maige-2017.jpg",
@@ -32,19 +32,19 @@ var trabajos = [{
     figure: "media/oma_lopez-maige-suarez-vallejos_2018.jpg",
     alt: "oma",
     title: "OMA",
-    about: "Ortesis, esqueleto de cobre y recubrimiento de cobre."
+    about: "Ortesis, esqueleto de cobre y recubrimiento de cobre.",
     url: 
 }, {
     figure: "media/estoy_lopez_2019.JPG",
     alt: "estoy",
     title: "estoy?",
-    about: "Acrílico sobre tela, 160x75cms."
+    about: "Acrílico sobre tela, 160x75cms.",
     url: 
 }, {
     figure: "media/fiori_lopez_2017.jpg",
     alt: "Fiori",
     title: "Fiori",
-    about: "Acuarela sobre papel, 45x21cms."
+    about: "Acuarela sobre papel, 45x21cms.",
     url:
     
 }];
@@ -54,5 +54,5 @@ $.each(trabajos, function(i, val) {
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
     var cuatro = '<p><a href= "' + trabajos[i].url + '" >Ver más</a><p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
